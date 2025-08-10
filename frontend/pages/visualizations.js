f399a1bba6f146d7e820fded40401598f463b394
Mohan Vamsi Krishna <myanamadala@binghamton.edu>
2025-08-09 23:25:03 -0700

    Update visualizations.js with Layout and placeholder

--- frontend/pages/visualizations.js
+++ frontend/pages/visualizations.js
@@ -1,1 +1,10 @@
-// TODO: implement visualizations page
+import Layout from '../components/Layout';
+
+export default function Visualizations() {
+  return (
+    <Layout>
+      <h1 className="text-3xl font-serif mb-4">Tariff Visualizations</h1>
+      <p className="text-lg">Charts will be added here in future updates.</p>
+    </Layout>
+  );
+}
